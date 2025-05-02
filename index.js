effe7a8149f4e5afd6ac7ca73d837c054e9f7537
Bashu7515 <bashushahg@gmail.com>
2025-05-03 04:03:27 +0500

    calculator

---
+++ index.js
@@ -0,0 +1,34 @@
+const buttonsEl = document.querySelectorAll("button");
+
+const inputFieldEl = document.getElementById("result");
+
+
+
+for(let i= 0; i < buttonsEl.length; i++){
+    buttonsEl[i].addEventListener("click", ()=>{
+        const buttonvalue = (buttonsEl[i].textContent);
+        if(buttonvalue == "C"){
+            clearResult();
+        }else if(buttonvalue =="="){
+            calculateResult();
+        }else{
+            appendValue(buttonvalue);
+        }
+    });
+}
+
+function clearResult(){
+
+inputFieldEl.value = "";
+
+}
+ function calculateResult(){
+
+
+    inputFieldEl.value = eval(inputFieldEl.value);
+ }
+ function appendValue(buttonvalue){
+    inputFieldEl.value += buttonvalue
+    // inputFieldEl.value = inputFieldEl.value + buttonvalue
+
+ }
